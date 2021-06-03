8084cf25f649ed5144175f25efac6cd2ddf5e9b1
enricofacca <enrico.facca@sns.it>
2021-06-03 09:27:15 +0200

    possible to use left and right prec. PCG included

--- apply_iterative_solver.m
+++ apply_iterative_solver.m
@@ -1,4 +1,4 @@
-function [sol,info]= apply_iterative_solver(matrix_operator, rhs, ctrl, prec,solini)
+function [sol,info]= apply_iterative_solver(matrix_operator, rhs, ctrl, prec,solini,left_right)
 
   if (exist('solini','var') )
     x0=solini;
@@ -7,34 +7,79 @@ function [sol,info]= apply_iterative_solver(matrix_operator, rhs, ctrl, prec,sol
   end
 
   info=info_solver;
+
+  if (~exist('left_right','var') )
+    left_right='left';
+  else
+    if ( strcmp(left_right,'left') || strcmp(left_right,'right') )
+      left_right=left_right;
+    else
+      disp('In apply_iterative_solver string left_right must be left or right')
+      return
+    end
+  end
   
   if (strcmp(ctrl.approach,'bicgstab'))
-	    [sol,i,j,info.iter] = bicgstab(@(y) matrix_operator(y),rhs,...
+    if (strcmp(left_right,'left'))
+	      [sol,info.flag,j,info.iter] = bicgstab(@(y) matrix_operator(y),rhs,...
+						     ctrl.tolerance,...
+						     ctrl.itermax,...
+						     @(x) prec(x),[],...% left  prec
+						     x0);
+    else
+      [sol,info.flag,j,info.iter] = bicgstab(@(y) matrix_operator(y),rhs,...
 					     ctrl.tolerance,...
 					     ctrl.itermax,...
-					     @(x) prec(x));
+					     [],@(x) prec(x),...% left  prec
+					     x0);
+    end
+
 	    
 	    info.res=norm(matrix_operator(sol)-rhs)/norm(rhs);
-	    info.flag = (info.res >= ctrl.tolerance);
+	    %info.flag = (info.res >= ctrl.tolerance);
+  elseif (strcmp(ctrl.approach,'pcg'))
+      
+    [sol,i,j,info.iter] = pcg(@(y) matrix_operator(y),rhs,...
+			      ctrl.tolerance,...
+			      ctrl.itermax,...
+			      @(x) prec(x));%,... 
+			      %x0);
+    info.flag=i;
+    info.res=norm(matrix_operator(sol)-rhs)/norm(rhs);
+    info.flag = (info.res >= ctrl.tolerance);
 	    
   elseif (strcmp(ctrl.approach,'fgmres'))
     [sol,i,res,iter_total] = fgmres(@(y,tol) matrix_operator(y),...
 				   rhs,ctrl.tolerance,...
 				   'max_iters',ctrl.itermax,...
 				   'restart',20,...
+				   'x0',x0,...
 				   'verb',0,...
-				   'P',@(x,tol) prec(x));
+				   'P',@(x,tol) prec(x)); %should be right prec
 
     info.iter=iter_total;
     info.res=norm(matrix_operator(sol)-rhs)/norm(rhs);
     info.flag = (info.res >= ctrl.tolerance);
 
   elseif (strcmp(ctrl.approach,'gmres'))
-    [sol,i,j,iters] = gmres(@(y) matrix_operator(y),rhs,...
-			    20, ctrl.tolerance,...
-			    ctrl.itermax,...
- 			    @(x) prec(x));
-    info.iter=iters(1);
+    if (strcmp(left_right,'left'))
+      [sol,i,j,iters] = gmres(@(y) matrix_operator(y),rhs,...
+			      20, ... % to fix the total number of iterattion
+			      ctrl.tolerance,...
+			      int64(ctrl.itermax/20),...
+ 			      @(x) prec(x),[],... %left ,right
+			      x0);      
+    else
+      [sol,i,j,iters] = gmres(@(y) matrix_operator(y),rhs,...
+			      20, ... % to fix the total number of iterattion
+			      ctrl.tolerance,...
+			      int64(ctrl.itermax/20),...
+ 			      [],@(x) prec(x),... %left,right
+			      x0);
+    end
+      
+    %fprintf('%d %d\n', iters(1),iters(2))
+    info.iter=(iters(1)-1)*20+iters(2);
     info.res=norm(matrix_operator(sol)-rhs)/norm(rhs);
     info.flag = (info.res >= ctrl.tolerance);
 
