a1e3e445e246c2b3f792aa764ed4132bd942fad9
enricofacca <enrico.facca@sns.it>
2021-06-08 13:20:44 +0200

    info in matrix vector operation

--- mxv_jacobian.m
+++ mxv_jacobian.m
@@ -1,19 +1,33 @@
-function [y] = mxv_jacobian(A,B1T,B2,C,ncellphi,x)
+function [y] = mxv_jacobian(x,A,B1T,B2,C,ncellphi,kernel,rhs)
   n=size(B1T,1);
   m=size(B1T,2);
 
   y=zeros(n+m,1);
 
   v=B1T*x(n+1:n+m);
+
+
   
+  verbose=0;
+  if (verbose)
+    print_imbalance(x(1:n),ncellphi)
+  end
+  
+  %y(1:n)     = A *projector(x(1:n),kernel) + v;
   y(1:n)     = A *x(1:n) + v;
   y(n+1:n+m) = B2*x(1:n) - C*x(n+1:n+m);
 
   imbalance_phi=0.0;
   imbalance_v=0.0;
-  for i = 1:int64(n/ncellphi)
-    imbalance_phi=max(imbalance_phi,sum(y((i-1)*ncellphi+1:i*ncellphi)));
-    imbalance_v=max(imbalance_v,sum(v((i-1)*ncellphi+1:i*ncellphi)));
+
+  if (verbose)
+    print_imbalance(y(1:n),ncellphi)
   end
-  %fprintf('max imbalance A x* B1T*y = %1.4e, imb B1T*y = %1.4e \n',imbalance_phi,imbalance_v);
+
+  if (verbose)
+    if (exist('rhs','var') )
+      fprintf(' res=%1.4e \n', norm(y-rhs)/norm(rhs))
+      print_imbalance(rhs(1:n),ncellphi)
+    end
+  end
 end
