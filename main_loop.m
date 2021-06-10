be13b02976905233a8fc38e719e55bfb56c40467
enricofacca <enrico.facca@sns.it>
2021-06-10 09:28:39 +0200

    including restart

--- main_loop.m
+++ main_loop.m
@@ -17,7 +17,9 @@ verb = 1; % verbosity level: {0,1,2}
 
 
 
-plot=0
+plot_figures=0
+
+restart=0;
 
 
 compute_eigen=0;
@@ -37,10 +39,10 @@ for mesh_type = 2
   % For each mesh, five levels of refinement h_i, 1->5, are available.
 
   % set here for 1 to 4
-  for h_i = 1:3
+  for h_i = 1:2
 	       % INCRESING TIME STEP
 	       % set here 1:5
-    for i=3
+    for i=1:4
       N=4*(2^(i-1))
       Nt = N+1;
 
@@ -127,7 +129,7 @@ for mesh_type = 2
 	  end
 	elseif (sol==10)
 	  % set here bicgstab,gmres,fgmres (for non stationary prec)
-	  ctrl_outer.init('fgmres',1e-5,1000);
+	  ctrl_outer.init('fgmres',1e-5,200);
 
 	  % preconditioner approach
 	  outer_prec='full'
@@ -160,7 +162,7 @@ for mesh_type = 2
 
    	    % set here from solvers
 	    for i=[4];%length(solvers)
-	      ctrl_inner11.init(solvers{i},1e-3,iters{i},1.0,1,label{i});
+	      ctrl_inner11.init(solvers{i},1e-12,iters{i},1.0,0,label{i});
 	      
 	      
 	      % store all controls
@@ -177,11 +179,17 @@ for mesh_type = 2
 				'relax4inv22',relax4inv22);
 	      
 
-	      approach_string=strcat('schurACwithdiagC_',...
+	      approach_string=strcat('grounded_schurACwithdiagC_',...
 				   ctrl_outer.approach,'_',...
 				   outer_prec,'_',...
 				   'invSAC',invS_approach{j},ctrl_inner11.label,'_',...
 				   'invC',ctrl_inner22.label);
+	      
+
+	      if (restart)
+		approach_string=strcat('restarted_',approach_string);
+	      end
+
 	      disp(approach_string)
               %approach_string=def_approach_string(controls);
 
@@ -195,7 +203,7 @@ for mesh_type = 2
 
 	  % set here fgmres (for non stationary prec), bicgstab,gmres, pcg
 	  for isolver=[3]%1:length(outer_solvers)
-	  ctrl_outer.init(outer_solvers{isolver},1e-10,3000,0.0,0); % verbose=[1,2] works only for fgmres
+	  ctrl_outer.init(outer_solvers{isolver},1e-5,3000,0.0,0); % verbose=[1,2] works only for fgmres
 
 	  
 	  % external prec appraoch
@@ -237,8 +245,8 @@ for mesh_type = 2
 	  label  ={'agmg1','agmg10','agmg100','direct','krylov1','krylov10','incomplete','diag'};
 	  relax4_inv22=0;
 	  
-	  for i=[8];%1:length(solvers)
-	    ctrl_inner22.init(solvers{i},1e-13,iters{i},1.0,1,label{i});
+	  for i=[1];%1:length(solvers)
+	    ctrl_inner22.init(solvers{i},1e-13,iters{i},1.0,0,label{i});
 	    controls = struct('indc',grounded_node,...
 			      'sol',solver_approach,...
 			      'outer_prec',outer_prec,...
@@ -326,9 +334,10 @@ for mesh_type = 2
 
 	    approach_string=strcat(ctrl_outer.approach,'_fullprecSCA_invA',label{i},'_invSCA',ctrl_inner22.label);
 	    disp(approach_string)
-	    geod;
+	    geod;	    
 	  end
-	end 
+	end
+
       end
     end
   end
