c61a55725c8d2583bec5b397388e3edc48ae8269
enricofacca <enrico.facca@sns.it>
2021-06-16 16:44:51 +0200

    proceduere to find global and local index for max diagonal of blocks Ai

--- set_grounding_node.m
+++ set_grounding_node.m
@@ -1,9 +1,11 @@
-function [indeces] = set_grounding_node(A,ncellphi)
+function [indeces_global,indeces_local] = set_grounding_node(A,ncellphi)
   Np=size(A,1);
-  indeces=zeros(fix(Np/ncellphi),1);
+  indeces_global=zeros(fix(Np/ncellphi),1);
+  indeces_global=zeros(fix(Np/ncellphi),1);
   for i=1:fix(Np/ncellphi)
     [amax,imax]=max(abs(spdiags(A(1+(i-1)*ncellphi:i*ncellphi,1+(i-1)*ncellphi:i*ncellphi),0)));
-    indeces(i)=(i-1)*ncellphi+imax;
-    %disp(indeces(i));
+    indeces_local(i)=imax;
+    indeces_global(i)=(i-1)*ncellphi+imax;
+    
   end
 end
