f51162e73b38c73b3e5360bf32271f0ce35ac56d
gptod <gabriele.tod93@gmail.com>
2021-06-22 16:59:58 +0200

    faster computation of harmonic reconstruction

--- drho_sig.m
+++ drho_sig.m
@@ -6,18 +6,36 @@ function [drhos] = drho_sig(ind,edges,mid,cc,rho)
 ncell = size(cc,1);
 nei = length(ind.internal);
 drhos = sparse(nei,ncell);
+ 
+% for e=1:nei
+%     
+%     K = edges(ind.internal(e),3);
+%     L = edges(ind.internal(e),4);
+%     dK = sqrt((mid(ind.internal(e),1)-cc(K,1))^2+(mid(ind.internal(e),2)-cc(K,2))^2);
+%     dL = sqrt((mid(ind.internal(e),1)-cc(L,1))^2+(mid(ind.internal(e),2)-cc(L,2))^2);
+%     ds = edges(ind.internal(e),5);
+%     % harmonic mean
+%     drhos(e,K) = drhos(e,K) + dKharm(rho(K),rho(L),dK,dL,ds);
+%     drhos(e,L) = drhos(e,L) + dKharm(rho(L),rho(K),dL,dK,ds);
+%         
+% end
 
-for e=1:nei
-    
-    K = edges(ind.internal(e),3);
-    L = edges(ind.internal(e),4);
-    dK = sqrt((mid(ind.internal(e),1)-cc(K,1))^2+(mid(ind.internal(e),2)-cc(K,2))^2);
-    dL = sqrt((mid(ind.internal(e),1)-cc(L,1))^2+(mid(ind.internal(e),2)-cc(L,2))^2);
-    ds = edges(ind.internal(e),5);
-    % harmonic mean
-    drhos(e,K) = drhos(e,K) + dKharm(rho(K),rho(L),dK,dL,ds);
-    drhos(e,L) = drhos(e,L) + dKharm(rho(L),rho(K),dL,dK,ds);
-        
-end
+
+KK=edges(ind.internal(:),3);
+LL=edges(ind.internal(:),4);
+
+% possono essere generati nella struttura ind
+indeK = sub2ind([nei ncell],[1:nei]',KK);
+indeL = sub2ind([nei ncell],[1:nei]',LL);
+
+% in futuro aggiungiamo il calcolo di dK e dL in edges
+dK = sqrt((mid(ind.internal(:),1)-cc(KK,1)).^2+...
+    (mid(ind.internal(:),2)-cc(KK,2)).^2);
+dL = sqrt((mid(ind.internal(:),1)-cc(LL,1)).^2+...
+    (mid(ind.internal(:),2)-cc(LL,2)).^2);
+ds = edges(ind.internal(:),5);
+
+drhos(indeK) = dKharm_vect(rho(KK),rho(LL),dK,dL,ds);
+drhos(indeL) = dKharm_vect(rho(LL),rho(KK),dL,dK,ds);
 
 
--- rho_sig.m
+++ rho_sig.m
@@ -2,19 +2,30 @@ function [rhos] = rho_sig(ind,edges,mid,cc,rho)
 
 % harmonic reconstruction from cells to edges
 
-nei = length(ind.internal);
-rhos = zeros(nei,1);
+% nei = length(ind.internal);
+% rhos = zeros(nei,1);
+% 
+% for e=1:nei
+%     
+%     K = edges(ind.internal(e),3);
+%     L = edges(ind.internal(e),4);
+%     dK = sqrt((mid(ind.internal(e),1)-cc(K,1))^2+(mid(ind.internal(e),2)-cc(K,2))^2);
+%     dL = sqrt((mid(ind.internal(e),1)-cc(L,1))^2+(mid(ind.internal(e),2)-cc(L,2))^2);
+%     ds = edges(ind.internal(e),5);
+%     % harmonic mean
+%     rhos(e) = rhos(e) + harm(rho(K),rho(L),dK,dL,ds);
+%     
+% end
 
-for e=1:nei
-    
-    K = edges(ind.internal(e),3);
-    L = edges(ind.internal(e),4);
-    dK = sqrt((mid(ind.internal(e),1)-cc(K,1))^2+(mid(ind.internal(e),2)-cc(K,2))^2);
-    dL = sqrt((mid(ind.internal(e),1)-cc(L,1))^2+(mid(ind.internal(e),2)-cc(L,2))^2);
-    ds = edges(ind.internal(e),5);
-    % harmonic mean
-    rhos(e) = rhos(e) + harm(rho(K),rho(L),dK,dL,ds);
 
-end
+KK=edges(ind.internal(:),3);
+LL=edges(ind.internal(:),4);
 
+% in futuro aggiungiamo il calcolo di dK e dL in edges
+dK = sqrt((mid(ind.internal(:),1)-cc(KK,1)).^2+...
+    (mid(ind.internal(:),2)-cc(KK,2)).^2);
+dL = sqrt((mid(ind.internal(:),1)-cc(LL,1)).^2+...
+    (mid(ind.internal(:),2)-cc(LL,2)).^2);
+ds = edges(ind.internal(:),5);
 
+rhos = harm_vect(rho(KK),rho(LL),dK,dL,ds);
