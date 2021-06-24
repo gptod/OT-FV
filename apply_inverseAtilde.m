b0c52605dad14a5cc72fbd3bd553a26bbc862fa9
enricofacca <enrico.facca@sns.it>
2021-06-24 12:25:44 +0200

    correction of tildeA inversion

--- apply_inverseAtilde.m
+++ apply_inverseAtilde.m
@@ -1,4 +1,4 @@
-function [sol]=apply_inverseAtilde(array_sparse_inverse,vectors,indeces_local,rhs)
+function [sol]=apply_inverseAtilde(array_sparse_inverse,Amatrix,indeces_global,indeces_local,rhs)
   nblocks=size(array_sparse_inverse,1);
   start=1;
   sol=zeros(size(rhs,1),1);
@@ -6,13 +6,30 @@ function [sol]=apply_inverseAtilde(array_sparse_inverse,vectors,indeces_local,rh
 
   %start
   nrow_block=size(array_sparse_inverse(1).matrix,1);
+  ncol_block=size(array_sparse_inverse(1).matrix,2);
+
+  total_ncol=ncol_block;
+  
   sol(1:nrow_block)=array_sparse_inverse(1).apply(rhs(1:nrow_block));
   start=nrow_block+1;
   for i=2:nblocks    
     %start
     nrow_block=size(array_sparse_inverse(i).matrix,1);
-    vec=rhs(start:start+nrow_block-1);
-    vec(indeces_local(i))=vec(indeces_local(i))-vectors(:,1+(i-2)*2)'*sol(start-nrow_block:start-1);
-    sol(start:start+nrow_block-1)=array_sparse_inverse(i).apply(vec);
+    ncol_block=size(array_sparse_inverse(i).matrix,2);
+
+    total_ncol=total_ncol+ncol_block;
+
+    % local rhs
+    local_rhs=rhs(start:start+nrow_block-1);
+    % taking the vector p1 taking the indeces_global(i)-row of block i,i-1
+    vec1=find(Amatrix(indeces_global(i),total_ncol-ncol_block+1:total_ncol))';
+
+    
+
+    % change rhs at local row with p1_i^x_i
+    local_rhs(indeces_local(i))=local_rhs(indeces_local(i))-vec1'*sol(start-nrow_block:start-1);
+
+    % solsve A_i x_i = local_rhs
+    sol(start:start+nrow_block-1)=array_sparse_inverse(i).apply(local_rhs);
     start=start+nrow_block;
   end
