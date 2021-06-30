b898354c13e0653693725bdaa5afa8d28f704729
enricofacca <enrico.facca@sns.it>
2021-06-30 16:20:37 +0200

    prec times matrix operation

---
+++ prec_times_matrix.m
@@ -0,0 +1,15 @@
+function out=prec_times_matrix(prec,matrix)
+
+  N=size(matrix,1);
+  M=size(matrix,2);
+  out=zeros(N,M);
+
+  fprintf('\b\b\b\b\b\b%4.2f ',0.0)
+  for i=1:M
+    if (mod(i,50)==0)
+      fprintf('\b\b\b\b\b\b%4.2f ',i/M*100)
+    end    
+    out(:,i)=prec(full(matrix(:,i)));
+  end
+  fprintf('\n')
+end
