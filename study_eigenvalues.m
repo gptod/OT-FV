f4be1567b5797de55d48992c7551616c36bf5906
enricofacca <enrico.facca@sns.it>
2021-07-07 11:34:55 +0200

    print

--- study_eigenvalues.m
+++ study_eigenvalues.m
@@ -11,4 +11,4 @@ function [eigenvalues]= study_eigenvalues(matrix,label,ID)
   fprintf(ID,'real part eigen %s : min=%1.4e max=%1.4e\n', label,min(real_part),max(real_part));
   fprintf(ID,'imag part eigen %s : min=%1.4e max=%1.4e\n', label,min(imag_part),max(imag_part));
   fprintf(ID,'abs       eigen %s : min=%1.4e max=%1.4e\n', label,min(abs_eig),max(abs_eig));
-  
+  fprintf(ID,'cond(%s)=%1.4e\n', label,max(abs_eig)/min(abs_eig));
