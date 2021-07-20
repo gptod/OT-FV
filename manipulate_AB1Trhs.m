4ab3f0dd03113b02f34970804e97694548ab0e76
enricofacca <enrico.facca@sns.it>
2021-07-20 18:50:14 +0200

    manipualtion by repalcing

--- manipulate_AB1Trhs.m
+++ manipulate_AB1Trhs.m
@@ -4,6 +4,7 @@ function [A,B1T,rhs,B1T_perturbation]= manipulate_AB1Trhs(A,B1T,rhs,indeces_glob
   Nt=N+1;
   ncellphi=size(A,1)/Nt;
   ncellrho=size(B1T,2)/N;
+  just_replace=0;
 
   B1T_perturbation=sparse(size(B1T,1),size(B1T,2));
   for i=2:Nt
@@ -16,21 +17,29 @@ function [A,B1T,rhs,B1T_perturbation]= manipulate_AB1Trhs(A,B1T,rhs,indeces_glob
 	    norm(vectors_x(:,1+(iblock-1)*2)),...
 	    norm(vectors_x(:,2+(iblock-1)*2)));
     end
-    %A(irow,1+(jblock-2)*ncellphi:(jblock-1)*ncellphi) =sparse(1,ncellphi);
+    if (just_replace)
+        A(irow,1+(jblock-2)*ncellphi:(jblock-1)*ncellphi) =sparse(1,ncellphi);
+	A(irow,1+(jblock-1)*ncellphi:jblock    *ncellphi) ==sparse(1,ncellphi);
+	B1T(irow,:)=sparse(1,size(B1T,2));
+	rhs(irow)=0;
+    end
+
+	
     A(irow,1+(jblock-2)*ncellphi:(jblock-1)*ncellphi) = ...
     A(irow,1+(jblock-2)*ncellphi:(jblock-1)*ncellphi)+...
     vectors_x(:,1+(iblock-1)*2)';
     
-    %A(irow,1+(jblock-1)*ncellphi:jblock    *ncellphi) ==sparse(1,ncellphi);
+    
+
     A(irow,1+(jblock-1)*ncellphi:jblock    *ncellphi) = ...
     A(irow,1+(jblock-1)*ncellphi:jblock    *ncellphi) + ...
     vectors_x(:,2+(iblock-1)*2)';
 
-    %B1T(irow,:)=sparse(1,size(B1T,2));
+    
     B1T(irow,:)=B1T(irow,:)+vectors_y(:,iblock)';
 
     B1T_perturbation(irow,:)=vectors_y(:,iblock)';
-    %rhs(irow)=0;
+    
     rhs(irow)=rhs(irow)+alphas(iblock);
   end
 
