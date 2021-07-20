783444f347f4654dec2353d68f564d0e7f2559c9
enricofacca <enrico.facca@sns.it>
2021-07-20 18:49:26 +0200

    B2 included in the inputs. not touch at this point

--- grounding.m
+++ grounding.m
@@ -1,13 +1,16 @@
-function [A,B1T,rhs]=grounding(A,B1T,rhs,inode,value)
+function [A,B1T,B2,rhs]=grounding(A,B1T,B2,rhs,inode,value)
   Np=size(A,1);
   Nr=size(B1T,2);
-  adiag=A(inode,inode);
-  adiag=1.0;
-  rhs(inode)=adiag*value;
-  A(inode,:)   = sparse(1,Np);
-  A(inode,inode)= adiag;
-  B1T(inode,:) = sparse(1,Nr);
+
+  %A(:,inode)=sparse(Np,1);
+  %B2(:,inode)=sparse(Nr,1);
+  
 
   
 
+  B1T(inode,:) = sparse(1,Nr);
+
+  A(inode,:)   = sparse(1,Np);
+  A(inode,inode)= 1.0;
+  rhs(inode)=value;
 end
