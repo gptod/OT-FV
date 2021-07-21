e6b5062dc68e0bfb247a5f97b5646dd2c969a352
enricofacca <enrico.facca@sns.it>
2021-07-21 09:28:17 +0200

    simpler indexing

--- manipulate_AB1Trhs.m
+++ manipulate_AB1Trhs.m
@@ -7,15 +7,14 @@ function [A,B1T,rhs,B1T_perturbation]= manipulate_AB1Trhs(A,B1T,rhs,indeces_glob
   just_replace=0;
 
   B1T_perturbation=sparse(size(B1T,1),size(B1T,2));
-  for i=2:Nt
-    iblock=i-1; %1
-    jblock=i;   %2
+  for i=1:N % cycle vectors
+    jblock=i+1;   %cycle block
     irow=indeces_global(jblock);
     if (0)
     fprintf('%d alphas(i)=%1.4e, norm(v1)=%1.4e norm(v2)=%1.4e \n',...
-	    iblock,alphas(iblock),...
-	    norm(vectors_x(:,1+(iblock-1)*2)),...
-	    norm(vectors_x(:,2+(iblock-1)*2)));
+	    iblock,alphas(i),...
+	    norm(vectors_x(:,1+(i-1)*2)),...
+	    norm(vectors_x(:,2+(i-1)*2)));
     end
     if (just_replace)
         A(irow,1+(jblock-2)*ncellphi:(jblock-1)*ncellphi) =sparse(1,ncellphi);
@@ -25,22 +24,22 @@ function [A,B1T,rhs,B1T_perturbation]= manipulate_AB1Trhs(A,B1T,rhs,indeces_glob
     end
 
 	
-    A(irow,1+(jblock-2)*ncellphi:(jblock-1)*ncellphi) = ...
-    A(irow,1+(jblock-2)*ncellphi:(jblock-1)*ncellphi)+...
-    vectors_x(:,1+(iblock-1)*2)';
+    A(irow,1+(i-1)*ncellphi:i*ncellphi) = ...
+    A(irow,1+(i-1)*ncellphi:i*ncellphi)+...
+    vectors_x(:,1+(i-1)*2)';
     
     
 
-    A(irow,1+(jblock-1)*ncellphi:jblock    *ncellphi) = ...
-    A(irow,1+(jblock-1)*ncellphi:jblock    *ncellphi) + ...
-    vectors_x(:,2+(iblock-1)*2)';
+    A(irow,1+(i)*ncellphi:(i+1)   *ncellphi) = ...
+    A(irow,1+(i)*ncellphi:(i+1)   *ncellphi) + ...
+    vectors_x(:,2+(i-1)*2)';
 
     
-    B1T(irow,:)=B1T(irow,:)+vectors_y(:,iblock)';
+    B1T(irow,:)=B1T(irow,:)+vectors_y(:,i)';
 
-    B1T_perturbation(irow,:)=vectors_y(:,iblock)';
+    B1T_perturbation(irow,:)=vectors_y(:,i)';
     
-    rhs(irow)=rhs(irow)+alphas(iblock);
+    rhs(irow)=rhs(irow)+alphas(i);
   end
 
 
