f2a684b2d7fd4166e61a5003d40fe38b8f28ffde
enricofacca <enrico.facca@sns.it>
2021-12-24 16:20:21 +0100

    included ps term

--- J_times_vectors.m
+++ J_times_vectors.m
@@ -3,7 +3,7 @@ function [res] = J_times_vectors(JF,d)
   Nr = size(JF.rr,1);
   Ns = size(JF.ss,1);
   res=zeros(Np+Nr+Ns,1);
-  res(1:Np)            =JF.pp*d(1:Np)      +JF.pr*d(1+Np:Np+Nr);
-  res(1+Np:Np+Nr)      =JF.rp*d(1:Np)      +JF.rr*d(1+Np:Np+Nr)+JF.rs*d(Np+Nr+1:Np+Nr+Ns);
+  res(1:Np)            =JF.pp*d(1:Np)      +JF.pr*d(1+Np:Np+Nr)+JF.ps*d(Np+Nr+1:Np+Nr+Ns);
+  res(Np+1:Np+Nr)      =JF.rp*d(1:Np)      +JF.rr*d(1+Np:Np+Nr)+JF.rs*d(Np+Nr+1:Np+Nr+Ns);
   res(Np+Nr+1:Np+Nr+Ns)=                    JF.sr*d(1+Np:Np+Nr)+JF.ss*d(Np+Nr+1:Np+Nr+Ns);
 end
