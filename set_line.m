b4e3bcdbe2307975907d5fdbf8cbfcef03834ce7
enrico facca <enrico.facca@inria.fr>
2022-09-06 12:09:47 +0200

    cleaning

--- set_line.m
+++
@@ -1,6 +0,0 @@
-function [matrix]=set_line(matrix,row,lcol,rcol,vector,add)
-  if (not(add))
-    matrix(row,lcol:rcol)=sparse(1:matrix(rcol-lcol+1));
-  end
-  matrix(row,lcol:rcol)=matrix(row,lcol:rcol)+vector;
-end
